2837254b63b576b25f481fe3f95cd557933967e8
Apolosmllb <minermllb@gmail.com>
2022-11-19 14:01:19 -0500

    update docker file

--- src/main/java/com/anonimos/springboot/app/lessors/clients/CarClientRest.java
+++ src/main/java/com/anonimos/springboot/app/lessors/clients/CarClientRest.java
@@ -1,12 +1,7 @@
 package com.anonimos.springboot.app.lessors.clients;
 
 import com.anonimos.springboot.app.lessors.models.Car;
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import com.fasterxml.jackson.datatype.jsr310.deser.LocalTimeDeserializer;
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalTimeSerializer;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,12 +9,10 @@ import java.util.List;
 @FeignClient(name = "msvc-cars", url = "${msvc.cars.url}")
 public interface CarClientRest {
 
-    @GetMapping("/id/{id}")
+    @GetMapping("/{id}")
     Car getById(@PathVariable Long id);
     @PostMapping(value = "/")
     Car create(@RequestBody Car car);
-
-
     @GetMapping(value="/cars-by-lessor")
     List<Car>  getCarsByLessor(@RequestParam Iterable<Long> ids);
 }
